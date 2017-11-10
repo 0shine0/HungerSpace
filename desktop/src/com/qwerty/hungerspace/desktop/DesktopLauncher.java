05d273a3883a60be60eaf0ca0c808de4affbe627
vampcat <areshhitman@gmail.com>
2017-11-11 01:18:11 +0530

    First commit

---
+++ desktop/src/com/qwerty/hungerspace/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.qwerty.hungerspace.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.qwerty.hungerspace.SpaceMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SpaceMain(), config);
+	}
+}
